75575762b95faf39bfa4eee1d0a107ff7ab9dfe6
genki-ist <genki.toyama@i-studio.co.jp>
2020-11-26 02:08:36 +0900

    add uwr samples

--- Assets/Scenes/Section3/FactoryMethods/ObservableWWWSample.cs
+++ Assets/Scenes/Section3/FactoryMethods/ObservableWWWSample.cs
@@ -13,8 +13,10 @@ namespace Samples.Section3.FactoryMethods
         {
             var uri = "https://unity3d.com/jp";
 
+            //結果のみ受け取る場合（deprecatedになっている）
             ObservableWWW.Get(uri).Subscribe( x => Debug.Log(x) );
 
+            //通信終了時にwwwごと受け取る場合（deprecatedになっている）
             // ObservableWWW.Get(uri).Subscribe( www => Debug.Log(www.text) );
         }
 
---
+++ Assets/Scenes/Section3/FactoryMethods/UnityWebRequestToObservable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+using UniRx;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class UnityWebRequestToObservable : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //UniTaskからobservableに変換
+            FetchAsync("https://unity.com/ja").ToObservable().Subscribe( x => Debug.Log(x) );
+        }
+
+        private async UniTask<string> FetchAsync(string uri)
+        {
+            using(var uwr = UnityWebRequest.Get(uri))
+            {
+                //awaitできる
+                await uwr.SendWebRequest();
+                return uwr.downloadHandler.text;
+            }
+        }
+    }
+}
