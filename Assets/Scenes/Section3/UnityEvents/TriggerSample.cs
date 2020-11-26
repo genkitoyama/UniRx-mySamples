fc7d21cf7c5c93f1db4c399668acaac8c9c87a8e
genki-ist <genki.toyama@i-studio.co.jp>
2020-11-27 02:14:02 +0900

    add some samples to section 3.5

---
+++ Assets/Scenes/Section3/UnityEvents/TriggerSample.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+//これを追加することで各イベントをobservableに変換できる
+using UniRx.Triggers;
+
+namespace Samples.Section3.FactoryMethods
+{
+    public class TriggerSample : MonoBehaviour
+    {
+        //自分とは別のgameobject
+        [SerializeField]
+        private GameObject childGameObject;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //自身のupdateをobservableに変換
+            this.UpdateAsObservable()
+                .Subscribe( _ => {
+                    this.transform.position += Vector3.forward * Time.deltaTime;
+                });
+
+            //他のgameobjectのonCollisionEnterをobservableに変換
+            childGameObject.OnCollisionEnterAsObservable()
+                            .Subscribe( collision => {
+                                Debug.Log("collide to " + collision.gameObject.name);
+                            }).AddTo(this); //他のgameobjectを利用するならAddToしたほうがよい
+
+            //自身のondesrtoyをobservableに変換
+            this.OnDestroyAsObservable()
+                .Subscribe( _ => {
+                    Debug.Log("destroyed");
+                });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scenes/Section3/uGUIs/UGuiEventConvert.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UniRx;
+
+namespace Samples.Section3.uGUIs
+{
+    public class UGuiEventConvert : MonoBehaviour
+    {
+        [SerializeField]
+        private Toggle toggle;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.toggle.isOn = false;
+
+            //uGUIイベントを変換するパターン
+            this.toggle.onValueChanged.AsObservable()
+                                      .Subscribe( x => Debug.Log("current state(AsObservable): " + x));
+
+            //拡張メソッド、subscribeした瞬間に初期値が発行される
+            this.toggle.OnValueChangedAsObservable()
+                       .Subscribe( x => Debug.Log("current state(augmented method): " + x));
+
+            Debug.Log("---");
+
+            this.toggle.isOn = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
