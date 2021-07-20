4087c557d05fadbe0a1007fc278e29296ad4d7a1
genkitoyama <genki.toyama.dev@gmail.com>
2021-07-20 16:08:16 +0900

    [update] add section5 mvrp

---
+++ Assets/Scenes/Section5/MVRP/CountModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+namespace Samples.Section5.MVRP
+{
+    public class CountModel : MonoBehaviour
+    {
+        [SerializeField]
+        private IntReactiveProperty current = new IntReactiveProperty(0);
+
+        //presenter向けに公開するプロパティ
+        public IReadOnlyReactiveProperty<int> Current => current;
+
+        //整数値の更新
+        public void UpdateCount(int value)
+        {
+            this.current.Value = Mathf.Clamp(value, 0, 100);
+        }
+    }
+
+}
